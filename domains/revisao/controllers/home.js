4b89b1eeed2dd7fed9476cc91fb3898f532ff3ab
João Pedro V <mastergamerjp06@gmail.com>
2025-09-06 13:20:58 -0300

    menus added

--- domains/revisao/controllers/home.js
+++ domains/revisao/controllers/home.js
@@ -1,24 +1,27 @@
 const Nayahath = require('../../../logs/ArcanaFlow')
 
 module.exports = async (req, res) => {
-            /*
-            Objetivo: Página inicial para acessar os recursos do módulo de revisão
-            Recebe: Nada
-            Retorna: Página inicial do módulo com nome, perfil e imagem de perfil do usuario
-            */
-            /*
-            ! Fluxo esperado
-            * Recebe a requisição e pega dados de login para exibição
-            * Retorna status 200 e renderiza a página inicial passando os dados de login
-            */
-           Nayahath.action('Revisão', 'Pediu homepage')
+    /*
+    Objetivo: Página inicial para acessar os recursos do módulo de revisão
+    Recebe: Nada
+    Retorna: Página inicial do módulo com nome, perfil e imagem de perfil do usuario
+    */
+    /*
+    ! Fluxo esperado
+    * Recebe a requisição e pega dados de login para exibição
+    * Retorna status 200 e renderiza a página inicial passando os dados de login
+    */
+    Nayahath.action('Revisão', 'Pediu homepage')
 
-           res.locals.currentPage = "revisao"
-            
-            const perfilUsuario = req.session.perfil;
-            const nomeUsuario = req.session.nomeUsuario;
-            const imagemPerfil = req.session.imagemPerfil;
+    res.locals.currentPage = "revisao"
 
-            // ! Temporário
-            res.redirect('/revisao/busca')
-        }
+    const { perfil, nomeUsuario, imagemPerfil} = req.session;
+
+    // ! Temporário
+    // res.redirect('/revisao/busca')
+    res.render('home', {
+        nomeUsuario,
+        perfilUsuario: perfil,
+        imagemPerfil
+    })
+}
