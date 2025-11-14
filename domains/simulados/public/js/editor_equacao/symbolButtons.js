3de70db3213bd1e81f41902a2d2795ac0f063c93
Ruan Patrick <114404700+ruan177@users.noreply.github.com>
2025-11-14 11:11:29 -0300

    Ajustes para produção

--- domains/simulados/public/js/editor_equacao/symbolButtons.js
+++ domains/simulados/public/js/editor_equacao/symbolButtons.js
@@ -324,7 +324,7 @@ const symbolButtons = [
         button: document.querySelector('#parentheses-matrix-btn'),
         value: '\\begin{pmatrix}a & b \\\\ c & d\\end{pmatrix} '
     },
-{
+    {
         button: document.querySelector('#brackets-matrix-nxn-btn'),
         value: '\\begin{bmatrix} a_{11} & a_{12} & \\cdots & a_{1n} \\\\ a_{21} & a_{22} & \\cdots & a_{2n} \\\\ \\vdots & \\vdots & \\ddots & \\vdots \\\\ a_{n1} & a_{n2} & \\cdots & a_{nn} \\end{bmatrix}'
     },
